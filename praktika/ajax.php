3f1b78fd978d4ebe5c47ef33f02be6045f6559a5
ramon123456 <garribillagaurbil@gmail.com>
2017-11-28 16:30:45 +0100

    sza praktika html

--- praktika/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['id'])) {
-	$bisitak = simplexml_load_file("/..data/bisita_liburua.xml");
-	echo "<tr><td>Iruzkina</td></tr> \n";
-	foreach($bisitak as $bisita) if($bisita['id'] == $_POST['id']) $iruzkina = $bisita->iruzkina;
-	echo "<tr><td>$iruzkina</td></tr> \n";
-}
-else echo('arazoak egon dira');
-?>
